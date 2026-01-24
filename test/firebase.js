e9632cc8c33942a8644fbefc9df04afa6b9c5fe7
veera4u <115456591+Vmfm@users.noreply.github.com>
2026-01-24 16:43:40 +0300

    Create firebase.js

---
+++ test/firebase.js
@@ -0,0 +1,14 @@
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyAlWP5D79PsX29H0hIZlfvL0YV5Us7v5I4",
+  authDomain: "sample-firebase-ai-app-1f408.firebaseapp.com",
+  databaseURL: "https://sample-firebase-ai-app-1f408-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sample-firebase-ai-app-1f408",
+  storageBucket: "sample-firebase-ai-app-1f408.firebasestorage.app",
+  messagingSenderId: "657942233322",
+  appId: "1:657942233322:web:a8dd9896ca8cfccdbb5793"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
